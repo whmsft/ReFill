040708803b0f9ff2a64f58c5815711e1fac59b44
whmsft <whmsft@outlook.com>
2024-01-24 17:44:31 +0530

    Made a basic box

--- build.bat
+++ build.bat
@@ -1,1 +1,3 @@
-gcc main.c -o main.exe -Wall -lraylib -lopengl32 -lgdi32 -lwinmm 
+del main.exe
+gcc main.c -o main.exe -Wall -lraylib -lopengl32 -lgdi32 -lwinmm
+main.exe
--- main.c
+++ main.c
@@ -1,32 +1,44 @@
 #include "./include/raylib.h"
 #if defined(PLATFORM_WEB)
-    #include <emscripten/emscripten.h>
+	#include <emscripten/emscripten.h>
 #endif
 
-const int screenWidth = 320;
-const int screenHeight = 640;
+int screenWidth, screenHeight;
 int value = 0;
 
 void UpdateDrawFrame(void);
 
 int main(void) {
-    SetConfigFlags(FLAG_WINDOW_RESIZABLE);
-    InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
-    #if defined(PLATFORM_WEB)
-        emscripten_set_main_loop(UpdateDrawFrame, 0, 1);
-    #else
-        SetTargetFPS(60);
-        while (!WindowShouldClose()) {
-            UpdateDrawFrame();
-        }
-    #endif
-    CloseWindow();
-    return 0;
+	SetConfigFlags(FLAG_WINDOW_RESIZABLE);
+	InitWindow(320, 640, "ReFill");
+	screenWidth  = 320; //GetScreenWidth();
+	screenHeight = 640; //GetScreenHeight();
+	#if defined(PLATFORM_WEB)
+		emscripten_set_main_loop(UpdateDrawFrame, 0, 1);
+	#else
+		SetTargetFPS(60);
+		while (!WindowShouldClose()) {
+			UpdateDrawFrame();
+		}
+	#endif
+	CloseWindow();
+	return 0;
 }
 
 void UpdateDrawFrame(void) {
-    BeginDrawing();
-        ClearBackground(RAYWHITE);
-        DrawText("Congrats! You created your first window!", 0, 0, 20, LIGHTGRAY);
-    EndDrawing();
+	// Update
+	if (value>100) value=0;
+	if (IsMouseButtonDown(MOUSE_BUTTON_LEFT)) {
+		value+=2;
+	}
+	// (IsMouseButtonReleased(MOUSE_BUTTON_LEFT))
+
+
+	// Draw
+	BeginDrawing();
+	ClearBackground(BLACK);
+	DrawRectangle(screenWidth/14, (screenHeight-screenWidth)+screenWidth/14, screenWidth*12/14, screenWidth*12/14, RAYWHITE);
+	DrawRectangle(screenWidth*1.5/14, (screenHeight-screenWidth)+screenWidth*1.5/14, screenWidth*11/14, screenWidth*11/14, BLACK);
+	DrawRectangle(screenWidth*2/14, (screenHeight-screenWidth)+screenWidth*(120-value)/140, screenWidth*10/14, screenWidth*value/140, RAYWHITE);
+	EndDrawing();
 }
