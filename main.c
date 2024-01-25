bc880cd496e73c3b5beb6ae4d396c3acea5dd1ac
whmsft <whmsft@outlook.com>
2024-01-25 17:49:41 +0530

    Pushed today's work

--- main.c
+++ main.c
@@ -3,16 +3,26 @@
 	#include <emscripten/emscripten.h>
 #endif
 
-int screenWidth, screenHeight;
-int value = 0;
+int screenWidth, screenHeight, targetValue;
+int wheelValue = 0;
+int gameScore = 0;
 
 void UpdateDrawFrame(void);
+
+int abs(int number) {
+	if (number>0) {
+		return number;
+	} else {
+		return number*-1;
+	}
+}
 
 int main(void) {
 	SetConfigFlags(FLAG_WINDOW_RESIZABLE);
 	InitWindow(320, 640, "ReFill");
 	screenWidth  = GetScreenWidth();
 	screenHeight = GetScreenHeight();
+	targetValue = GetRandomValue(1,49)*2;
 	#if defined(PLATFORM_WEB)
 		emscripten_set_main_loop(UpdateDrawFrame, 0, 1);
 	#else
@@ -27,18 +37,21 @@ int main(void) {
 
 void UpdateDrawFrame(void) {
 	// Update
-	if (value>100) value=0;
-	if (IsMouseButtonDown(MOUSE_BUTTON_LEFT)) {
-		value+=2;
+	if (wheelValue>100) wheelValue=0;
+	if (IsMouseButtonDown(MOUSE_BUTTON_LEFT)) wheelValue+=2;
+	if (IsMouseButtonReleased(MOUSE_BUTTON_LEFT)) {
+		wheelValue = 0;
+		if (abs(wheelValue-targetValue)<10) {
+			targetValue = GetRandomValue(1,49)*2;
+			gameScore += 1;
+		}
 	}
-	// (IsMouseButtonReleased(MOUSE_BUTTON_LEFT))
-
 
 	// Draw
 	BeginDrawing();
 	ClearBackground(BLACK);
 	DrawRectangle(screenWidth/14, (screenHeight-screenWidth)+screenWidth/14, screenWidth*12/14, screenWidth*12/14, RAYWHITE);
 	DrawRectangle(screenWidth*1.5/14, (screenHeight-screenWidth)+screenWidth*1.5/14, screenWidth*11/14, screenWidth*11/14, BLACK);
-	DrawRectangle(screenWidth*2/14, (screenHeight-screenWidth)+screenWidth*(120-value)/140, screenWidth*10/14, screenWidth*value/140, RAYWHITE);
+	DrawRectangle(screenWidth*2/14, (screenHeight-screenWidth)+screenWidth*(120-wheelValue)/140, screenWidth*10/14, screenWidth*wheelValue/140, RAYWHITE);
 	EndDrawing();
 }
